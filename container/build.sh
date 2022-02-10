c16db0b4ab5e00bb5eb79cdd280eb7d6de6f0ef9
Antoon Huiskens <a.huiskens@f5.com>
2022-02-10 22:30:09 +0100

    Issue 0: build.sh purges container images on ID

--- container/build.sh
+++ container/build.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-
-podman rmi demos
-podman rmi public.ecr.aws/y6q2t0j9/demos
+IMAGE=demos
+podman rmi $(podman image -f reference=${IMAGE} --format={{.ID}})
 aws ecr-public get-login-password --region us-east-1 | podman login --username AWS --password-stdin public.ecr.aws/y6q2t0j9
 podman build -t demos .
 set -x
