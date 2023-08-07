04fb0c6a101221e30702720378878f44c25ea7ac
codecowboydotio <scottvankalken@gmail.com>
2023-08-07 10:15:25 +1000

    Basic multicast server

--- multicast-sync/go.mod
+++ multicast-sync/go.mod
@@ -1,3 +1,5 @@
 module github.com/tetsuo-dev/tetsuo.dev-code/multicast-sync
 
 go 1.16
+
+require github.com/libp2p/go-libp2p v0.29.1 // indirect
--- multicast-sync/main.go
+++ multicast-sync/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
     "fmt"
+    "os"
+    "os/signal"
+    "syscall"
     "github.com/libp2p/go-libp2p"
 )
 
@@ -14,6 +17,12 @@ func main() {
 
     // print the node's listening addresses
     fmt.Println("Listen addresses:", node.Addrs())
+
+    // wait for a SIGINT or SIGTERM signal
+    ch := make(chan os.Signal, 1)
+    signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+    <-ch
+    fmt.Println("Received signal, shutting down...")
 
     // shut the node down
     if err := node.Close(); err != nil {
