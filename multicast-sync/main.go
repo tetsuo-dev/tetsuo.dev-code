c2e2d677a7b02ca8553e3ae16c4ffd6141ea3de6
codecowboydotio <scottvankalken@gmail.com>
2023-08-12 11:39:56 +1000

    updating p2p sync

--- multicast-sync/main.go
+++ multicast-sync/main.go
@@ -2,27 +2,20 @@ package main
 
 import (
     "fmt"
-    "os"
-    "os/signal"
-    "syscall"
     "github.com/libp2p/go-libp2p"
 )
 
 func main() {
     // start a libp2p node with default settings
-    node, err := libp2p.New()
+    node, err := libp2p.New(
+    libp2p.ListenAddrStrings("/ip4/0.0.0.0/tcp/0"),
+    )
     if err != nil {
         panic(err)
     }
 
     // print the node's listening addresses
     fmt.Println("Listen addresses:", node.Addrs())
-
-    // wait for a SIGINT or SIGTERM signal
-    ch := make(chan os.Signal, 1)
-    signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
-    <-ch
-    fmt.Println("Received signal, shutting down...")
 
     // shut the node down
     if err := node.Close(); err != nil {
