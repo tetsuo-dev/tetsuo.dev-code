bbebde07e50f8626bef1e3d83756028796462cae
codecowboydotio <scottvankalken@gmail.com>
2023-08-12 11:42:59 +1000

    waiting for sigint

--- multicast-sync/main.go
+++ multicast-sync/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+    "os"
+    "os/signal"
+    "syscall"
     "fmt"
     "github.com/libp2p/go-libp2p"
 )
 
 func main() {
-    // start a libp2p node with default settings
+    // start a libp2p node that listens on all addresses and a random port each time.
     node, err := libp2p.New(
     libp2p.ListenAddrStrings("/ip4/0.0.0.0/tcp/0"),
     )
@@ -16,6 +19,12 @@ func main() {
 
     // print the node's listening addresses
     fmt.Println("Listen addresses:", node.Addrs())
+
+    // wait for a SIGINT or SIGTERM signal
+    ch := make(chan os.Signal, 1)
+    signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+    <-ch
+    fmt.Println("Received signal, shutting down...")
 
     // shut the node down
     if err := node.Close(); err != nil {
